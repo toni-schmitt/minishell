dcc739e42d2bc8565eac1d4f3208b7389a6d12e1
tblaase <tblaase@student.42heilbronn.de>
2021-10-23 17:09:08 +0200

    started adding code to pwd - makefile not working -code itself is working

--- src/main.c
+++ src/main.c
@@ -6,31 +6,14 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:28:36 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/23 16:54:16 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/23 17:05:53 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// void	ft_pwd(char *cwd)
-// {
-// 	// char	*cwd;
-
-// 	// cwd = NULL;
-// 	cwd = getcwd(cwd, 0);
-// 	if (cwd == NULL)
-// 		perror("Error with getcwd");
-// 	printf("%s", cwd);
-// }
 
 int	main(void)
 {
-	// char	*tst;
-
-	// tst = NULL;
-	// ft_pwd(tst);
+	pwd();
 	return (EXIT_SUCCESS);
 }
--- src/pwd.c
+++ src/pwd.c
@@ -6,21 +6,18 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:22:09 by tblaase           #+#    #+#             */
-/*   Updated: 2021/10/23 16:52:08 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/23 17:06:05 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <stdlib.h>
 
-void	ft_pwd(char *cwd)
+void	pwd(void)
 {
-	// char	*cwd;
+	char	*cwd;
 
-	// cwd = NULL;
-	getcwd(cwd, 10);
+	cwd = NULL;
+	cwd = getcwd(cwd, 0);
 	if (cwd == NULL)
 		perror("Error with getcwd");
 	printf("%s", cwd);
