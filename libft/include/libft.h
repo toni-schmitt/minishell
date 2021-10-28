1cb4469b4624bba41b3b76109148033aa49dac92
tblaase <tblaase@student.42heilbronn.de>
2021-10-28 20:55:32 +0200

    added ft_realloc_str to libft

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 17:23:59 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/28 19:48:33 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/28 20:53:48 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*realloc_str(char *str, size_t size);
 
 /* READ / WRITE */
 char	*get_next_line(int fd);
---
+++ libft/src/mem/ft_realloc_str.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc_str.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 19:55:37 by tblaase           #+#    #+#             */
+/*   Updated: 2021/10/28 20:52:47 by tblaase          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_realloc_str(char *str, size_t size)
+/*
+this function is similar to realloc, but only for strings
+++ instead of malloc this uses calloc
+-- it will create a new adress and free the old one of str
+if allocation of the new size fails str wont be freed
+if size is smaller than str, str will get returned
+str: adress of the string you want to realloc
+size: number of elements you want to realloc
+new: adress of the reallocated string
+*/
+{
+	char	*new;
+	int		i;
+
+	if (size < ft_strlen(str))
+		return (str);
+	i = 0;
+	new = ft_calloc(size, sizeof(char));
+	if (!new)
+		return (NULL);
+	while (str && str[i])
+	{
+		new[i] = str[i];
+		i++;
+	}
+	ft_free(str);
+	return (new);
+}
