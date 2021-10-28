e73696d1365772ae9f9cc931e5c525201aca4b04
tblaase <tblaase@student.42heilbronn.de>
2021-10-28 17:52:38 +0200

    ready for merge with inbuilt branch - removed main from header - restored main to default

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:29:02 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/26 22:27:19 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/28 17:51:20 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include <errno.h>
 # include <dirent.h>
 
-int	main(int argc, char **argv);
 int	cd(char **argv);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,13 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:28:36 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/26 22:27:40 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/28 17:51:09 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	(void)argc;
-	if (cd(argv) == 1)
-		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
