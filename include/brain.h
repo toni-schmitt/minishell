558b437a963f3267274bb051a4e6940ea2cd7273
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-10-29 16:36:02 +0200

    change prototypes back to non conflicting types

--- include/brain.h
+++ include/brain.h
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 22:21:31 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/29 16:17:08 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/10/29 16:35:12 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <readline/readline.h>
 
 // BRAIN
-int		lexer(char *line);
-int		parser(char **tokens);
+int		lexer(char *envp[]);
+int		parser(void);
 int		expander(void);
 int		executor(void);
 
