ec0c6edd0c7f7c00bbff7065ca7bc6f0bbcf49f0
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-10-29 14:35:12 +0200

    adjust syntax, add malloc protection - changed syntax to better fit with other libft functions - added malloc protection - added ft_str_arr_len function instead of counting in function - removed printf statement

--- libft/src/string/ft_str_arr_dup.c
+++ libft/src/string/ft_str_arr_dup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_arr_dup.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 18:12:28 by tblaase           #+#    #+#             */
-/*   Updated: 2021/10/28 19:52:01 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/10/29 14:33:04 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,11 @@ cpy: the copied array that gets returned
 	char	**cpy;
 	int		i;
 
-	if (!arr)
+	if (arr == NULL)
 		return (NULL);
-	i = 0;
-	while (arr[i])
-		i++;
-	cpy = ft_calloc(i + 1, sizeof(char *));
-	printf("%d\n", i);
+	cpy = ft_calloc(ft_str_arr_len(arr) + 1, sizeof(*cpy));
+	if (cpy == NULL)
+		return (NULL);
 	i = 0;
 	while (arr[i])
 	{
