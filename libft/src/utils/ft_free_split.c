0a955ac88e2b75505c84506ea357d499be35b169
Toni Schmitt <tonischmitt@online.de>
2021-10-30 16:14:17 +0200

    free split now returns NULL

--- libft/src/utils/ft_free_split.c
+++ libft/src/utils/ft_free_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_split.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 17:05:35 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/16 16:30:34 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/10/30 16:14:04 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 /*
  * Frees allocated array returned by ft_split function.
  * Assumes that ft_split successfully split the string.
+ * Returns NULL
 */
-void	ft_free_split(char **split)
+void	*ft_free_split(char **split)
 {
 	int	i;
 
@@ -27,4 +28,5 @@ void	ft_free_split(char **split)
 		i++;
 	}
 	free(split);
+	return (NULL);
 }
