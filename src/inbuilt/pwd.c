a6757d2f2dfa213755dcd09ca513c4faa1266af9
tblaase <tblaase@student.42heilbronn.de>
2021-11-04 20:05:00 +0100

    fixed memory leaks of pwd

--- src/inbuilt/pwd.c
+++ src/inbuilt/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:22:09 by tblaase           #+#    #+#             */
-/*   Updated: 2021/10/28 18:02:31 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/11/04 17:34:03 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ int	pwd(void)
 
 	cwd = NULL;
 	cwd = getcwd(cwd, 0);
-	if (cwd == NULL)
+	if (cwd == NULL) //cwd can never be NULL????
 		perror("Error with getcwd");
 	if (printf("%s\n", cwd) != ft_strlen(cwd) + 1)
+	{
+		ft_free_str(&cwd);
 		return (EXIT_FAILURE);
+	}
+	ft_free_str(&cwd);
 	return (EXIT_SUCCESS);
 }
