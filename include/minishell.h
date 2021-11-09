4357238b11dabcf650bb3875e16f390d61db6986
tblaase <tblaase@student.42heilbronn.de>
2021-11-09 18:13:48 +0100

    cd not leaking anymore -still needs testing if everything works as expected -still needs to be normed

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:29:02 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/03 13:33:34 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/09 18:08:37 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@
 # include <dirent.h>
 # include <stdbool.h>
 
+typedef struct s_environment
+{
+	char	**envp;
+	char	**env_var;
+}			t_environment;
+
 /* INBUILD */
 int		echo(char **args);
 char	**export(char **argv, char **env_var);
 char	**unset(char **argv, char **env_var);
 int		env(char **envp);
 int		pwd(void);
-int		cd(char **argv, char **envp, char **env_var);
+int		cd(char **argv, t_environment *environment);
 
 #endif
--- src/inbuilt/cd.c
+++ src/inbuilt/cd.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 14:12:17 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/03 22:13:22 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/09 18:11:04 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ static char	*find_home(char **envp, char **env_var)
 	char	*path;
 
 	i = 0;
+	path = NULL;
 	while (env_var && env_var[i] && ft_strcmp(env_var[i], "HOME=") != 0)
 		i++;
-	if (!env_var || ft_strlen(env_var[i]) <= 6) // env_var[i] == NULL not needed because strlen <= 6 will protect for that case as well
+	if (!env_var || ft_strlen(env_var[i]) <= 5) // env_var[i] == NULL not needed because strlen <= 6 will protect for that case as well
 	{
 		i = 0;
 		while (envp[i])
@@ -33,48 +34,87 @@ static char	*find_home(char **envp, char **env_var)
 			i++;
 		}
 	}
-	return (ft_strdup(env_var[i]));
+	else
+		path = ft_strdup(ft_strchr(env_var[i], '=') + 1);
+	return (path);
 }
 
-int	cd(char **argv, char **envp, char **env_var)
+static void	ft_update_env_var(t_environment *environment, char *var, char *value)
+{
+	int	i;
+
+	i = 0;
+	while (environment->env_var && environment->env_var[i])
+	{
+		if (ft_strcmp(environment->env_var[i], var) == 0 && (environment->env_var[i][ft_strlen(var)] == '=' || environment->env_var[i][ft_strlen(var)] == '\0'))
+		{
+			ft_free_str(&environment->env_var[i]);
+			environment->env_var[i] = ft_strstrjoin(var, value, "=");
+			return ;
+		}
+		i++;
+	}
+	if (environment->env_var && environment->env_var[i] == NULL)
+	{
+		environment->env_var = ft_realloc_str_arr(environment->env_var, ft_str_arr_len(environment->env_var) + 1);
+		environment->env_var[i] = ft_strstrjoin(var, value, "=");
+	}
+}
+
+static void	ft_free_cd_work(char **home, char **cwd)
+{
+	ft_free_str(cwd);
+	ft_free_str(home);
+}
+
+int	cd(char **argv, t_environment *environment)
 {
 	char	*home;
-	char	**split;
-	int		i;
+	char	*cwd;
+	DIR		*directory;
 
-	home = find_home(envp, env_var);
-printf("homefolder found:%s\n", home);
+	cwd = NULL;
+	home = NULL;
+	cwd = getcwd(cwd, 0);
+	ft_update_env_var(environment, ft_strdup("OLDPWD"), cwd);
+	ft_free_cd_work(&home, &cwd);
+	home = find_home(environment->envp, environment->env_var);
+// printf("homefolder found:%s\n", home);
+// (void)argv;
 	if (argv[1] == NULL)
 	{
 		if (chdir(home) != 0)
 		{
 			perror("Error");
-			return (1);
+			ft_free_cd_work(&home, &cwd);
+			return (EXIT_FAILURE);
 		}
-		else
-			return (0);
+		cwd = getcwd(cwd, 0);
+		ft_update_env_var(environment, ft_strdup("PWD"), cwd);
+		ft_free_cd_work(&home, &cwd);
+		return (EXIT_SUCCESS);
 	}
-	if (ft_strchr(argv[1], '~') != NULL)
-	{
-		// argv[1] = home; // need to refine that depending on the real input, probably leaks
-		split = ft_split(argv[1], '~'); // dont need to split, i need to replace every occurance of it with home
-	printf("split: %s, %s, %s\n", split[0], split[1], split[2]);
-		i = 0;
-		while (split[i + 1])
-		{
-			split[0] = ft_strstrjoin(split[0], split[i++], home);
-		printf("argv[1]: %s\n", argv[1]);
-		}
-	}
-	else if (opendir(argv[1]) == NULL && errno != 0)
+	directory = opendir(argv[1]);
+	if (directory == NULL || access(argv[1], F_OK) !=0)
 	{
 		perror("Error");
-		return (1);
+		ft_free_cd_work(&home, &cwd);
+		return (EXIT_FAILURE);
 	}
-	if (chdir(argv[1]) != 0)
+	else if (closedir(directory) != 0)
 	{
 		perror("Error");
-		return (1);
+		ft_free_cd_work(&home, &cwd);
+		return (EXIT_FAILURE);
 	}
-	return (0);
+	else if (chdir(argv[1]) != 0)
+	{
+		perror("Error");
+		ft_free_cd_work(&home, &cwd);
+		return (EXIT_FAILURE);
+	}
+	cwd = getcwd(cwd, 0);
+	ft_update_env_var(environment, ft_strdup("PWD"), cwd);
+	ft_free_cd_work(&home, &cwd);
+	return (EXIT_SUCCESS);
 }
