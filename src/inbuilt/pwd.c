f4946e469782484ccb917dbe5abc607b70e10acc
tblaase <tblaase@student.42heilbronn.de>
2021-11-09 18:29:23 +0100

    pwd is working and no leaks

--- src/inbuilt/pwd.c
+++ src/inbuilt/pwd.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:22:09 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/04 17:34:03 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/09 18:27:13 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	pwd(void)
 
 	cwd = NULL;
 	cwd = getcwd(cwd, 0);
-	if (cwd == NULL) //cwd can never be NULL????
-		perror("Error with getcwd");
-	if (printf("%s\n", cwd) != ft_strlen(cwd) + 1)
+	if (cwd == NULL)
+		perror("Error");
+	if (ft_printf("%s\n", cwd) != ft_strlen(cwd) + 1)
 	{
 		ft_free_str(&cwd);
 		return (EXIT_FAILURE);
