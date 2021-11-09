d48a7346858d5f11660cf43e17a754a4f55bd46b
tblaase <tblaase@student.42heilbronn.de>
2021-11-09 18:43:40 +0100

    env now uses the environment struct -everything works as intended -no leaks found

--- src/inbuilt/env.c
+++ src/inbuilt/env.c
@@ -6,20 +6,20 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 13:48:30 by tblaase           #+#    #+#             */
-/*   Updated: 2021/10/28 17:29:17 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/09 18:37:16 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	env(char **envp)
+int	env(t_environment *environment)
 {
 	int	i;
 
 	i = 0;
-	while (envp[i] != NULL)
+	while (environment->env_var && environment->env_var[i] != NULL)
 	{
-		if (printf("%s\n", envp[i++]) == 0)
+		if (ft_printf("%s\n", environment->env_var[i++]) == 0)
 			return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
