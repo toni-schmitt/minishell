a456f717482e768c7d15cf6c8a794751207380c6
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-11-16 21:21:46 +0100

    add protection against NULL-string

--- libft/src/string/ft_strdup.c
+++ libft/src/string/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 14:09:09 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/03 19:20:10 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/11/16 21:14:01 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@ char	*ft_strdup(const char *str)
 	int		duped_len;
 
 	duped_len = ft_strlen(str);
+	if (duped_len == 0)
+		return (NULL);
 	duped = malloc((duped_len + 1) * sizeof(*duped));
 	if (duped == NULL)
 		return (NULL);
-	while (*str)
+	while (str && *str)
 		*duped++ = *str++;
 	*duped = '\0';
 	return (duped - duped_len);
