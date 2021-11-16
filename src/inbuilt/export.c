4adec1d23180cbedd35eda932bf3077cb155839a
tblaase <tblaase@student.42heilbronn.de>
2021-11-16 22:16:09 +0100

    fixed a memory leak and added protection

--- src/inbuilt/export.c
+++ src/inbuilt/export.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:49:07 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/16 19:52:19 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/16 21:56:41 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static int	ft_export_found(t_environment *env, t_export *exp, char **argv)
 {
 	if (ft_strcmp(env->env_var[exp->i], exp->var) == 0)
 	{
+		ft_free_str(&env->env_var[exp->i]);
 		env->env_var[exp->i] = ft_strdup(argv[exp->j++]);
 		ft_free_str(&exp->var);
 		if (env->env_var[exp->i] == NULL)
@@ -40,16 +41,29 @@ static int	ft_export_new(t_environment *env, t_export *exp, char **argv)
 	return (EXIT_SUCCESS);
 }
 
+static void	ft_split_var(t_export *exp, char **argv)
+{
+	char	**var_split;
+
+	var_split = ft_split(argv[exp->j], '=');
+	if (var_split == NULL)
+		exp->var = NULL;
+	else
+	{
+		exp->var = ft_strdup(var_split[0]);
+		ft_free_str_array(&var_split);
+	}
+}
+
 static int	ft_export_loop(t_environment *env, t_export *exp, char **argv)
 {
 	int		check;
-	char	**var_split;
 
 	while (argv[exp->j])
 	{
-		var_split = ft_split(argv[exp->j], '=');
-		exp->var = ft_strdup(var_split[0]);
-		ft_free_str_array(&var_split);
+		ft_split_var(exp, argv);
+		if (exp->var == NULL)
+			return (EXIT_FAILURE);
 		exp->i = 0;
 		while (env->env_var[exp->i])
 		{
@@ -73,6 +87,8 @@ int	export(char **argv, t_environment *env)
 {
 	t_export	*exp;
 
+	if (env == NULL || env->env_var == NULL)
+		return (EXIT_FAILURE);
 	if (argv[1] == NULL)
 		export_only(env);
 	else
