edad98aaf62ec4a72204cbf93ae9d22fc191f322
tblaase <tblaase@student.42heilbronn.de>
2021-11-19 20:32:08 +0100

    changed cd (WIP) - was not having the intended behaviour - segfaulted in certain scenarios - still WIP (line 105)

--- src/inbuilt/cd.c
+++ src/inbuilt/cd.c
@@ -6,13 +6,13 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 14:12:17 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/10 20:07:36 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/19 19:30:00 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*find_home(char **envp, char **env_var)
+static char	*find_home(char **env_var)
 {
 	int		i;
 	char	*path;
@@ -21,21 +21,11 @@ static char	*find_home(char **envp, char **env_var)
 	path = NULL;
 	while (env_var && env_var[i] && ft_strcmp(env_var[i], "HOME=") != 0)
 		i++;
-	if (!env_var || ft_strlen(env_var[i]) <= 5)
-	{
-		i = 0;
-		while (envp[i])
-		{
-			if (ft_strcmp(envp[i], "HOME=") == 0)
-			{
-				path = ft_strdup(ft_strchr(envp[i], '=') + 1);
-				return (path);
-			}
-			i++;
-		}
-	}
+	if (env_var[i] == NULL || ft_strlen(env_var[i]) <= 5)
+		ft_printf("cd: HOME not set\n");
 	else
 		path = ft_strdup(ft_strchr(env_var[i], '=') + 1);
+	// printf("HOME FOUND:%s\n", path);
 	return (path);
 }
 
@@ -67,7 +57,6 @@ char *value)
 
 static int	ft_exit_cd(char **home, char **cwd, int exit_status)
 {
-	printf("%d\n", exit_status);
 	ft_free_str(cwd);
 	ft_free_str(home);
 	if (exit_status == EXIT_SUCCESS)
@@ -79,16 +68,16 @@ static int	ft_exit_cd(char **home, char **cwd, int exit_status)
 	}
 }
 
-static int	ft_cd_home(t_environment *environment, char **home)
+static int	ft_cd_home(t_environment *environment, char **home_path)
 {
 	char	*cwd;
 
 	cwd = NULL;
-	if (chdir(*home) != 0)
-		return (ft_exit_cd(home, &cwd, 1));
+	if (chdir(*home_path) != 0)
+		return (ft_exit_cd(home_path, &cwd, 1));
 	cwd = getcwd(cwd, 0);
 	ft_update_env_var(environment, ft_strdup("PWD"), cwd);
-	return (ft_exit_cd(home, &cwd, 0));
+	return (ft_exit_cd(home_path, &cwd, EXIT_SUCCESS));
 }
 
 // check why it prints error when running ./minishell
@@ -96,25 +85,35 @@ static int	ft_cd_home(t_environment *environment, char **home)
 int	cd(char **argv, t_environment *environment)
 {
 	char	*cwd;
-	char	*home;
+	char	*home_path;
 	DIR		*directory;
 
 	cwd = NULL;
-	home = NULL;
+	home_path = NULL;
+	if (argv == NULL)
+		return (EXIT_FAILURE);
 	cwd = getcwd(cwd, 0);
 	ft_update_env_var(environment, ft_strdup("OLDPWD"), cwd);
-	ft_exit_cd(&home, &cwd, 0);
-	home = find_home(environment->envp, environment->env_var);
+	ft_exit_cd(&home_path, &cwd, 0);
+	// home_path = find_home(environment->env_var);
+	// printf("path to home is:%s\n", home);
 	if (argv[1] == NULL)
-		ft_cd_home(environment, &home);
+	{
+		home_path = find_home(environment->env_var);
+		if (home_path == NULL)
+		{
+			//fix exit of the programm right here
+		}
+		return (ft_cd_home(environment, &home_path));
+	}
 	directory = opendir(argv[1]);
 	if (directory == NULL)
-		return (ft_exit_cd(&home, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, 1));
 	else if (closedir(directory) != 0)
-		return (ft_exit_cd(&home, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, 1));
 	else if (chdir(argv[1]) != 0)
-		return (ft_exit_cd(&home, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, 1));
 	cwd = getcwd(cwd, 0);
 	ft_update_env_var(environment, ft_strdup("PWD"), cwd);
-	return (ft_exit_cd(&home, &cwd, 0));
+	return (ft_exit_cd(&home_path, &cwd, 0));
 }
