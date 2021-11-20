bb77dd138ed69330a25a2736516d7f852f08b059
tblaase <tblaase@student.42heilbronn.de>
2021-11-20 12:48:48 +0100

    cd changed (WIP) - prep to store PWD and OLDPWD outside of env_var as well in case you unset them

--- src/inbuilt/cd.c
+++ src/inbuilt/cd.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 14:12:17 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/19 19:30:00 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/20 12:16:45 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,33 +25,32 @@ static char	*find_home(char **env_var)
 		ft_printf("cd: HOME not set\n");
 	else
 		path = ft_strdup(ft_strchr(env_var[i], '=') + 1);
-	// printf("HOME FOUND:%s\n", path);
 	return (path);
 }
 
-static void	ft_update_env_var(t_environment *environment, char *var,
+static void	ft_update_env_var(t_env *envv, char *var,
 char *value)
 {
 	int	i;
 
 	i = 0;
-	while (environment->env_var && environment->env_var[i])
+	while (envv->env_var && envv->env_var[i])
 	{
-		if (ft_strcmp(environment->env_var[i], var) == 0
-			&& (environment->env_var[i][ft_strlen(var)] == '='
-			|| environment->env_var[i][ft_strlen(var)] == '\0'))
+		if (ft_strcmp(envv->env_var[i], var) == 0
+			&& (envv->env_var[i][ft_strlen(var)] == '='
+			|| envv->env_var[i][ft_strlen(var)] == '\0'))
 		{
-			ft_free_str(&environment->env_var[i]);
-			environment->env_var[i] = ft_strstrjoin(var, value, "=");
+			ft_free_str(&envv->env_var[i]);
+			envv->env_var[i] = ft_strstrjoin(var, value, "=");
 			return ;
 		}
 		i++;
 	}
-	if (environment->env_var && environment->env_var[i] == NULL)
+	if (envv->env_var && envv->env_var[i] == NULL)
 	{
-		environment->env_var = ft_realloc_str_arr(environment->env_var,
-				ft_str_arr_len(environment->env_var) + 1);
-		environment->env_var[i] = ft_strstrjoin(var, value, "=");
+		envv->env_var = ft_realloc_str_arr(envv->env_var,
+				ft_str_arr_len(envv->env_var) + 1);
+		envv->env_var[i] = ft_strstrjoin(var, value, "=");
 	}
 }
 
@@ -68,21 +67,20 @@ static int	ft_exit_cd(char **home, char **cwd, int exit_status)
 	}
 }
 
-static int	ft_cd_home(t_environment *environment, char **home_path)
+static int	ft_cd_home(t_env *envv, char **home_path, char **cwd)
 {
-	char	*cwd;
-
-	cwd = NULL;
 	if (chdir(*home_path) != 0)
-		return (ft_exit_cd(home_path, &cwd, 1));
-	cwd = getcwd(cwd, 0);
-	ft_update_env_var(environment, ft_strdup("PWD"), cwd);
-	return (ft_exit_cd(home_path, &cwd, EXIT_SUCCESS));
+		return (ft_exit_cd(home_path, cwd, 1));
+	ft_update_env_var(envv, ft_strdup("OLDPWD"), *cwd);
+	ft_free_str(cwd);
+	*cwd = getcwd(*cwd, 0);
+	ft_update_env_var(envv, ft_strdup("PWD"), *cwd);
+	return (ft_exit_cd(home_path, cwd, EXIT_SUCCESS));
 }
 
 // check why it prints error when running ./minishell
 
-int	cd(char **argv, t_environment *environment)
+int	cd(char **argv, t_env *envv)
 {
 	char	*cwd;
 	char	*home_path;
@@ -92,28 +90,25 @@ int	cd(char **argv, t_environment *environment)
 	home_path = NULL;
 	if (argv == NULL)
 		return (EXIT_FAILURE);
-	cwd = getcwd(cwd, 0);
-	ft_update_env_var(environment, ft_strdup("OLDPWD"), cwd);
-	ft_exit_cd(&home_path, &cwd, 0);
-	// home_path = find_home(environment->env_var);
-	// printf("path to home is:%s\n", home);
 	if (argv[1] == NULL)
 	{
-		home_path = find_home(environment->env_var);
+		home_path = find_home(envv->env_var);
 		if (home_path == NULL)
-		{
-			//fix exit of the programm right here
-		}
-		return (ft_cd_home(environment, &home_path));
+			return (EXIT_FAILURE);
 	}
+	cwd = getcwd(cwd, 0);
+	if (argv[1] == NULL)
+		return (ft_cd_home(envv, &home_path, &cwd));
 	directory = opendir(argv[1]);
 	if (directory == NULL)
-		return (ft_exit_cd(&home_path, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
 	else if (closedir(directory) != 0)
-		return (ft_exit_cd(&home_path, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
 	else if (chdir(argv[1]) != 0)
-		return (ft_exit_cd(&home_path, &cwd, 1));
+		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
+	ft_update_env_var(envv, ft_strdup("OLDPWD"), cwd);
+	ft_free_str(&cwd);
 	cwd = getcwd(cwd, 0);
-	ft_update_env_var(environment, ft_strdup("PWD"), cwd);
+	ft_update_env_var(envv, ft_strdup("PWD"), cwd);
 	return (ft_exit_cd(&home_path, &cwd, 0));
 }
