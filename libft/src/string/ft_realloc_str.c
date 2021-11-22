6289b27fcf4565fcd8139c7885774d371808506c
tblaase <tblaase@student.42heilbronn.de>
2021-11-22 19:05:36 +0100

    fixed ft_realloc_str to match the bahavior of ft_realloc_str_arr

--- libft/src/string/ft_realloc_str.c
+++ libft/src/string/ft_realloc_str.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 19:55:37 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/02 16:16:55 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/22 19:04:03 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 this function is similar to realloc, but only for strings
 ++ instead of malloc this uses calloc
 -- it will create a new adress and free the old one of str
-if allocation of the new size fails str wont be freed
-if size is <= to the size of str, str will get returned
+if allocation of the new size fails str will be freed
 str: adress of the string you want to realloc
 size: number of elements you want to realloc
 new: adress of the reallocated string
@@ -29,13 +28,14 @@ char	*ft_realloc_str(char *str, size_t size)
 
 	if (str == NULL)
 		return (NULL);
-	if (size <= (size_t)ft_strlen(str))
-		return (str);
-	new = ft_calloc(size, sizeof(*new));
+	new = ft_calloc(size + 1, sizeof(*new));
 	if (new == NULL)
+	{
+		free(str);
 		return (NULL);
+	}
 	i = 0;
-	while (str && str[i])
+	while (str && size--)
 	{
 		new[i] = str[i];
 		i++;
