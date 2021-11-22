e7d35bb388428e2806ae8bc89e92c522a44589c6
tblaase <tblaase@student.42heilbronn.de>
2021-11-22 17:01:28 +0100

    changed the behavior - now allocates and free regardless of the change in size and the success

--- libft/src/string/ft_realloc_str_arr.c
+++ libft/src/string/ft_realloc_str_arr.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 20:35:01 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/10 13:43:09 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/22 16:57:02 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 this function is similar to realloc, but only for string-arrays
 ++ instead of malloc this uses calloc
 -- it will create a new adress and free the old one of arr
-if allocation of the new size fails arr wont be freed
-if size is <= to the size of arr, arr will get returned
+if allocation of the new size fails arr will be freed
 arr: adress of the array you want to realloc
 size: number of elements you want to realloc
 new: adress of the reallocated array of strings
@@ -27,15 +26,16 @@ char	**ft_realloc_str_arr(char **arr, size_t size)
 	char	**new;
 	int		i;
 
-	if (arr == NULL || *arr == NULL)
+	if (arr == NULL)
 		return (NULL);
-	if (size <= (size_t)ft_str_arr_len(arr))
-		return (arr);
 	new = ft_calloc(size + 1, sizeof(*new));
 	if (new == NULL)
+	{
+		ft_free_str_array(&arr);
 		return (NULL);
+	}
 	i = 0;
-	while (arr && arr[i])
+	while (size-- && arr)
 	{
 		new[i] = ft_strdup(arr[i]);
 		i++;
