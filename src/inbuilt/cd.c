445a52cb4c98009f349af2a9038ce0feab662c8f
tblaase <tblaase@student.42heilbronn.de>
2021-11-22 20:47:40 +0100

    cd now working as intended (finished) - no memory leaks found - i think everything is protected - fully intentional behavior

--- src/inbuilt/cd.c
+++ src/inbuilt/cd.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 14:12:17 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/22 17:01:54 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/22 20:39:14 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ static char	*find_home(char **env_var)
 {
 	char	*path;
 
-	path = search_env_var(env_var, "HOME");
+	if (env_var == NULL)
+		return (NULL);
+	path = get_env_var_value(env_var, "HOME");
 	if (path == NULL || ft_strlen(path) == 0)
 		ft_printf("cd: HOME not set\n");
+	if (ft_strlen(path) == 0)
+		ft_free_str(&path);
 	return (path);
 }
 
@@ -27,30 +31,32 @@ static void	ft_update_env_var(t_env *envv, char *var, char *value)
 	int	i;
 
 	i = 0;
+	if (ft_strcmp(var, "OLDPWD") == 0)
+	{
+		ft_free_str(&envv->oldpwd);
+		envv->oldpwd = ft_strdup(value);
+	}
+	if (ft_strcmp(var, "PWD") == 0)
+	{
+		ft_free_str(&envv->pwd);
+		envv->pwd = ft_strdup(value);
+	}
 	while (envv->env_var && envv->env_var[i])
 	{
-		if (ft_strncmp(envv->env_var[i], var, ft_strlen(var)) == 0
-			&& (envv->env_var[i][ft_strlen(var)] == '='
-			|| envv->env_var[i][ft_strlen(var)] == '\0'))
+		if (ft_strcmp(envv->env_var[i], var) == 0
+			|| ft_strcmp(envv->env_var[i], var) == 61)
 		{
 			ft_free_str(&envv->env_var[i]);
-			envv->env_var[i] = ft_strstrjoin(var, value, "=");
+			envv->env_var[i] = ft_strstrjoin(ft_strdup(var), value, "=");
 			return ;
 		}
 		i++;
 	}
-	if (envv->env_var && envv->env_var[i] == NULL)
-	{
-		envv->env_var = ft_realloc_str_arr(envv->env_var,
-				ft_str_arr_len(envv->env_var) + 1);
-		envv->env_var[i] = ft_strstrjoin(var, value, "=");
-	}
 }
 
-static int	ft_exit_cd(char **home, char **cwd, int exit_status)
+static int	ft_exit_cd(char **cwd, int exit_status)
 {
 	ft_free_str(cwd);
-	ft_free_str(home);
 	if (exit_status == EXIT_SUCCESS)
 		return (EXIT_SUCCESS);
 	else
@@ -60,49 +66,54 @@ static int	ft_exit_cd(char **home, char **cwd, int exit_status)
 	}
 }
 
-static int	ft_cd_home(t_env *envv, char **home_path, char **cwd)
+static int	ft_cd_home(t_env *envv)
 {
-	if (chdir(*home_path) != 0)
-		return (ft_exit_cd(home_path, cwd, 1));
-	ft_update_env_var(envv, ft_strdup("OLDPWD"), *cwd);
-	ft_free_str(cwd);
-	*cwd = getcwd(*cwd, 0);
-	ft_update_env_var(envv, ft_strdup("PWD"), *cwd);
-	return (ft_exit_cd(home_path, cwd, EXIT_SUCCESS));
+	char	*home_path;
+	char	*cwd;
+
+	cwd = NULL;
+	home_path = find_home(envv->env_var);
+	if (home_path == NULL)
+		return (EXIT_FAILURE);
+	cwd = getcwd(cwd, 0);
+	if (chdir(home_path) != 0)
+	{
+		ft_free_str(&home_path);
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
+	}
+	ft_update_env_var(envv, "OLDPWD", cwd);
+	ft_free_str(&cwd);
+	cwd = getcwd(cwd, 0);
+	ft_update_env_var(envv, "PWD", cwd);
+	ft_free_str(&home_path);
+	if (envv->pwd == NULL || envv->oldpwd == NULL)
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
+	return (ft_exit_cd(&cwd, EXIT_SUCCESS));
 }
 
 int	cd(char **argv, t_env *envv)
 {
 	char	*cwd;
-	char	*home_path;
 	DIR		*directory;
 
-	cwd = NULL;
-	home_path = NULL;
-	if (argv == NULL)
+	if (argv == NULL || envv == NULL)
 		return (EXIT_FAILURE);
 	if (argv[1] == NULL)
-	{
-		home_path = find_home(envv->env_var); // maybe include this content of the if statement into the find home function to save some lines
-		if (home_path == NULL || ft_strlen(home_path) < 1)
-		{
-			ft_free_str(&home_path);
-			return (EXIT_FAILURE);
-		}
-		cwd = getcwd(cwd, 0);
-		return (ft_cd_home(envv, &home_path, &cwd));
-	}
+		return (ft_cd_home(envv));
+	cwd = NULL;
 	cwd = getcwd(cwd, 0);
 	directory = opendir(argv[1]);
 	if (directory == NULL)
-		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
 	else if (closedir(directory) != 0)
-		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
 	else if (chdir(argv[1]) != 0)
-		return (ft_exit_cd(&home_path, &cwd, EXIT_FAILURE));
-	ft_update_env_var(envv, ft_strdup("OLDPWD"), cwd);
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
+	ft_update_env_var(envv, "OLDPWD", cwd);
 	ft_free_str(&cwd);
 	cwd = getcwd(cwd, 0);
-	ft_update_env_var(envv, ft_strdup("PWD"), cwd);
-	return (ft_exit_cd(&home_path, &cwd, 0));
+	ft_update_env_var(envv, "PWD", cwd);
+	if (envv->pwd == NULL || envv->oldpwd == NULL)
+		return (ft_exit_cd(&cwd, EXIT_FAILURE));
+	return (ft_exit_cd(&cwd, EXIT_SUCCESS));
 }
