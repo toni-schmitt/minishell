38596ca51876bb28108d68635efd489b4fe97b66
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-11-26 23:10:20 +0100

    add brain.h header

--- src/brain/expander.c
+++ src/brain/expander.c
@@ -6,13 +6,14 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:39:06 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/04 16:25:35 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/11/24 22:20:28 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "brain.h"
 
-int	expander(t_parser_tok *parser_tokens)
+int	expander(t_par_tok *parser_tokens[])
 {
 	(void)parser_tokens;
 	return (EXIT_FAILURE);
