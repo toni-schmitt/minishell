2ba52bfdb9938da49d72df8e617ad231c97d17fc
tblaase <tblaase@student.42heilbronn.de>
2021-11-26 14:12:15 +0100

    back to standard without comment

--- src/main.c
+++ src/main.c
@@ -6,19 +6,13 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:28:36 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/02 15:52:45 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/26 14:11:57 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	main(void)
-/*
-FOR EXPORT and UNSET:
-- main function needs to set **env_var equal to envp at the beginning!!!!!!!!
-- export should allways be called like env_var = export(argv, env_var)
-  so it updates env_var if some changes are needed
-*/
 {
 	return (EXIT_SUCCESS);
 }
