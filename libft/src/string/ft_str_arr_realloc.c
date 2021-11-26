89517e6c79232beeee93a7bf4c5f7c70e1558ee4
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-11-26 23:08:56 +0100

    add protections - now reallocates always correct

--- libft/src/string/ft_str_arr_realloc.c
+++ libft/src/string/ft_str_arr_realloc.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 16:39:04 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/18 17:44:06 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/11/26 19:48:54 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ char	**ft_str_arr_realloc(char *array[], size_t size)
 	char	**reallocated;
 	int		i;
 
-	i = 0;
-	if (array == NULL)
-		return (NULL);
 	reallocated = ft_calloc(size + 1, sizeof(*reallocated));
 	if (reallocated == NULL)
 		return (NULL);
-	while (array[i])
+	i = 0;
+	while (size-- && array && array[i])
 	{
 		reallocated[i] = ft_strdup(array[i]);
 		i++;
