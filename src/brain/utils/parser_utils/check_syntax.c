fc8112ba84329147e20d2e58e5871f0131360c0a
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-11-26 23:13:15 +0100

    init check_syntax function

---
+++ src/brain/utils/parser_utils/check_syntax.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_syntax.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 20:28:49 by tschmitt          #+#    #+#             */
+/*   Updated: 2021/11/26 23:13:10 by tschmitt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "brain.h"
+#include "minishell.h"
+#include "parser_utils.h"
+
+int	check_syntax(t_par_tok *par_toks[])
+{
+	(void)par_toks;
+	return (EXIT_FAILURE);
+}
