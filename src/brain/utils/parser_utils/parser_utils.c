92b71cb67884a15eefbbe098f31fadedd07b4e6c
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-11-29 18:47:30 +0100

    change init_token to init_curr_par_tok - now automatically allocates current parser token - sets iterator back to default values - now returns int for error checking

--- src/brain/utils/parser_utils/parser_utils.c
+++ src/brain/utils/parser_utils/parser_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 20:05:39 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/27 02:01:09 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/11/29 18:42:22 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 #include "minishell.h"
 #include "parser_utils.h"
 
-void	init_token(t_par_tok **par_tok)
+int	init_curr_par_tok(void)
 {
-	t_iter	*iter;
+	t_par_tok	**par_toks;
+	t_iter		*iter;
 
-	if (*par_tok != NULL)
-		return ;
+	par_toks = get_par_toks();
 	iter = get_iter();
+	if (par_toks[iter[par]] != NULL)
+		return (EXIT_SUCCESS);
 	iter[cmd] = 0;
 	iter[in] = 0;
 	iter[out] = 0;
-	*par_tok = ft_calloc(1 + 1, 100);
-	if (*par_tok == NULL)
-		return ;
+	par_toks[iter[par]] = ft_calloc(1 + 1, sizeof(*par_toks[iter[par]]));
+	if (par_toks[iter[par]] == NULL)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
 int	free_parser(t_par_tok *par_tok[], t_iter *iter, int exit_status)
@@ -81,7 +84,8 @@ int	get_subshell_tok(t_iter *iter)
 	char		*lex_tok;
 	t_par_tok	*par_tok;
 
-	init_token(&get_par_toks()[get_iter()[par]]);
+	if (init_curr_par_tok() == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	lex_tok = get_curr_lex_tok();
 	par_tok = get_curr_par_tok();
 	par_tok->cmd_size++;
