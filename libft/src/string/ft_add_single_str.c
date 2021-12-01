3d1be3d15ce2bb80f08708476302819645720bb4
tblaase <tblaase@student.42heilbronn.de>
2021-12-01 16:19:55 +0100

    removed unnecessary stuff from ft_add_single_str - added two comments (need to take a closer look)

--- libft/src/string/ft_add_single_str.c
+++ libft/src/string/ft_add_single_str.c
@@ -6,33 +6,29 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 22:17:58 by tblaase           #+#    #+#             */
-/*   Updated: 2021/11/30 22:52:30 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/01 16:18:43 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+add new after (*arr)[pos]
+*/
 char	**ft_add_single_str(char ***arr, int pos, char *new)
 {
 	int		i;
 
-	i = 0;
+	pos++;
 	if (new == NULL)
 	{
 		ft_free_str_array(arr);
 		return (NULL);
 	}
-	ft_realloc_str_arr(*arr, ft_str_arr_len(*arr) + 1);
+	*arr = ft_realloc_str_arr(*arr, ft_str_arr_len(*arr) + 1);
 	if (*arr == NULL)
 		return (NULL);
-	while (*arr && (*arr)[i] && i < pos)
-		i++;
-	if (*arr == NULL || (*arr)[i] == NULL)
-	{
-		ft_free_str_array(arr);
-		return (NULL);
-	}
-	i = ft_str_arr_len(*arr) + 1;
+	i = ft_str_arr_len(*arr);
 	while (i > pos)
 	{
 		(*arr)[i] = (*arr)[i - 1];
