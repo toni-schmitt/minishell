99db70845f599a60df039ccfa3645d5645d411ae
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-12-02 15:28:30 +0100

    remove waiting for heredoc from main

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:28:36 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/11/27 01:46:20 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:27:44 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,43 +25,6 @@ static int	exit_routine(void *to_free, int exit_status)
 	return (exit_status);
 }
 
-static char	*get_nosebang(char *line)
-{
-	char	*nosebang;
-	char	**arr;
-
-	arr = ft_split_set(ft_strstr(line, "<<") + 2, " \t\n\v\f\r");
-	if (arr == NULL)
-		return (NULL);
-	nosebang = ft_strdup(arr[0]);
-	if (nosebang == NULL)
-		return (NULL);
-	ft_free_split(arr);
-	return (nosebang);
-}
-
-static int	wait_for_nosebang(char *line)
-{
-	char	*buf;
-	char	*nosebang;
-
-	nosebang = get_nosebang(line);
-	if (nosebang == NULL)
-		return (EXIT_FAILURE);
-	while (true)
-	{
-		buf = readline("> ");
-		if (ft_strncmp(buf, nosebang, ft_strlen(buf)) == 0)
-		{
-			free(nosebang);
-			free(buf);
-			return (EXIT_SUCCESS);
-		}
-		free(buf);
-	}
-	return (EXIT_FAILURE);
-}
-
 static int	routine(void)
 {
 	char	*buf;
@@ -71,11 +34,6 @@ static int	routine(void)
 		buf = readline("minishell$ ");
 		if (ft_strlen(buf) > 0)
 			add_history(buf);
-		if (ft_strstr(buf, "<<"))
-		{
-			if (wait_for_nosebang(buf) == EXIT_FAILURE)
-				return (exit_routine((void *)buf, EXIT_FAILURE));
-		}
 		if (lexer(buf) == EXIT_FAILURE)
 			return (exit_routine((void *)buf, EXIT_FAILURE));
 		free(buf);
