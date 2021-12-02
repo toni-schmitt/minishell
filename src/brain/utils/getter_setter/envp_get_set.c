5567fa0f11cf64b8b8844dec291fe1f07d57799b
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-12-02 21:22:08 +0100

    change style to match other getter / setter

--- src/brain/utils/getter_setter/envp_get_set.c
+++ src/brain/utils/getter_setter/envp_get_set.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   envp_utils.c                                       :+:      :+:    :+:   */
+/*   envp_get_set.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 16:12:12 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/10/29 16:17:54 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/12/02 21:20:35 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 static char	**_get_envp(char *envp[])
 {
-	static char	**static_envp;
+	static char	**static_envp = NULL;
 
-	if (envp != NULL)
-		static_envp = envp;
+	if (envp == NULL)
+		return (static_envp);
+	static_envp = envp;
 	return (static_envp);
 }
 
