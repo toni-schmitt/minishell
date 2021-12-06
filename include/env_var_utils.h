638ecbdbd8154d1e257886e581fad0584b128f89
Toni Schmitt <tonischmitt@online.de>
2021-12-06 19:39:45 +0100

    add env_var_utils header - for enviroment variables - for envv getter setter

---
+++ include/env_var_utils.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_var_utils.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 17:52:36 by toni              #+#    #+#             */
+/*   Updated: 2021/12/06 19:32:59 by toni             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENV_VAR_UTILS_H
+# define ENV_VAR_UTILS_H
+
+/* TRANSPORT OF ENV_VARIABLES */
+typedef struct s_env
+{
+	char	**envp;
+	char	**env_var;
+	char	*pwd;
+	char	*oldpwd;
+}			t_env;
+
+// ENV VAR UTILS
+t_env	*init_envv(char **envp);
+void	free_envv(t_env **envv);
+int		reinit_env_var(t_env *envv, char **argv);
+char	*get_env_var_value(t_env *envv, char *var);
+
+// GETTER / SETTER
+void	set_envv(t_env *envv);
+t_env	*get_envv(void);
+char	**get_env_var(void);
+
+#endif
