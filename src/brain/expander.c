d79a3cbe199f93bed47b1e33e618ef7803c8722e
tblaase <tblaase@student.42heilbronn.de>
2021-12-08 18:00:48 +0100

    now executor only gets callde if paths of in and out redirections are correct

--- src/brain/expander.c
+++ src/brain/expander.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:39:06 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/08 13:56:49 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/08 17:56:21 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,8 +86,11 @@ int	expander(t_par_tok *par_toks[])
 	if (get_tokens(par_toks) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	exp_toks = get_exp_toks();
-	handle_redir(par_toks, exp_toks); // here we need to set the exit_status to 1 if something went wrong and somehow quit the executor
-	if (executor(exp_toks) == EXIT_FAILURE)
+	// if (handle_redir(par_toks, exp_toks) == EXIT_FAILURE) // here we need to set the exit_status to 1 if something went wrong and somehow quit the executor
+	// {
+	// 	handle_wrong_path();
+	// }
+	if (handle_redir(par_toks, exp_toks) == EXIT_SUCCESS && executor(exp_toks) == EXIT_FAILURE)
 	{
 		free_exp_toks(exp_toks, EXIT_FAILURE);
 		return (EXIT_FAILURE);
--- src/brain/utils/expander_utils/handle_redirs_utils.c
+++ src/brain/utils/expander_utils/handle_redirs_utils.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:51:20 by tblaase           #+#    #+#             */
-/*   Updated: 2021/12/08 14:01:42 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/08 17:58:32 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "brain.h"
 #include "expander_utils.h"
 
-static void	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
+static int	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
 {
 	int		i;
 	int		fd;
@@ -23,13 +23,14 @@ static void	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
 	fd = 0;
 	while (true)
 	{
-		if (par_tok->redir_type[is_in] || par_tok->redir_type[is_pipe])
+		i++;
+		if (par_tok->redir_type[is_in])
 			fd = open(par_tok->in[i], O_RDONLY);
 		if (fd == -1)
 		{
 			printf("something with %s is wrong\n", par_tok->in[i]);
-			perror("ERROR_in");
-			return ; // here we need to set the exit_status to 1 and somehow quit the executor
+			perror("ERROR_in");// change this after finished debugging to ERROR
+			return (EXIT_FAILURE); // here we need to set the exit_status to 1 and somehow quit the executor
 		}
 		if (par_tok->in[i + 1] == NULL)
 			break ;
@@ -37,9 +38,10 @@ static void	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
 	}
 	exp_tok->in = fd;
 	printf("the new fd for input is now %d\n", exp_tok->in);
+	return (EXIT_SUCCESS);
 }
 
-static void	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
+static int	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
 {
 	int		i;
 	int		fd;
@@ -48,16 +50,16 @@ static void	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
 	fd = 1;
 	while (true)
 	{
-		if (par_tok->redir_type[is_pipe])
-			fd = -2;
-		if (par_tok->redir_type[is_out])
+		if (par_tok->redir_type[is_out]
+			&& ft_strcmp(par_tok->out[i++], ">") == 0)
 			fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_TRUNC, 0644);
-		else if (par_tok->redir_type[is_out_append])
+		else if (par_tok->redir_type[is_out_append]
+			&& ft_strcmp(par_tok->out[i++], ">>") == 0)
 			fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_APPEND, 0644);
 		if (fd == -1)
 		{
-			perror("ERROR_out");
-			return ; // here we need to set the exit_status to 1 and somehow quit the executor
+			perror("ERROR_out");// change this after finished debugging to ERROR
+			return (EXIT_FAILURE); // here we need to set the exit_status to 1 and somehow quit the executor
 		}
 		if (par_tok->out[i + 1] == NULL)
 			break ;
@@ -65,9 +67,10 @@ static void	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
 	}
 	exp_tok->out = fd;
 	printf("the new fd for output is now %d\n", exp_tok->out);
+	return (EXIT_SUCCESS);
 }
 
-void	handle_redir(t_par_tok **par_toks, t_exp_tok **exp_toks)
+int	handle_redir(t_par_tok **par_toks, t_exp_tok **exp_toks)
 {
 	int	i;
 
@@ -75,9 +78,16 @@ void	handle_redir(t_par_tok **par_toks, t_exp_tok **exp_toks)
 	while (par_toks && par_toks[i] && exp_toks && exp_toks[i])
 	{
 		if (par_toks[i]->in && par_toks[i]->in[0])
-			open_in(par_toks[i], exp_toks[i]);
+		{
+			if (open_in(par_toks[i], exp_toks[i]) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+		}
 		if (par_toks[i]->out && par_toks[i]->out[0])
-			open_out(par_toks[i], exp_toks[i]);
+		{
+			if (open_out(par_toks[i], exp_toks[i]) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+		}
 		i++;
 	}
+	return (EXIT_SUCCESS);
 }
