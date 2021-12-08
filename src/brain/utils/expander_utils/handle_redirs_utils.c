db276e7857502cca704ed32f4b7f46474efa30e6
tblaase <tblaase@student.42heilbronn.de>
2021-12-08 13:06:19 +0100

    normed handle_redirs_utils

--- src/brain/utils/expander_utils/handle_redirs_utils.c
+++ src/brain/utils/expander_utils/handle_redirs_utils.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:51:20 by tblaase           #+#    #+#             */
-/*   Updated: 2021/12/08 12:58:57 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/08 13:05:58 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,73 @@
 #include "brain.h"
 #include "expander_utils.h"
 
-static int	open_all_redirs(t_par_tok *par_tok, t_exp_tok *exp_tok)
+static int	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
 {
 	int		i;
 	int		fd;
 
 	i = 0;
 	fd = 0;
-	if (par_tok->in[i] != NULL)
+	while (true)
 	{
-		while (true)
+		if (par_tok->redir_type[is_in] || par_tok->redir_type[is_pipe])
+			fd = open(par_tok->in[i], O_RDONLY);
+		if (fd == -1)
 		{
-			if (par_tok->redir_type[is_in] || par_tok->redir_type[is_pipe])
-				fd = open(par_tok->in[i], O_RDONLY);
-			if (fd == -1)
-			{
-				perror("ERROR");
-				return (errno);
-			}
-			if (par_tok->in[i + 1] == NULL)
-			{
-				exp_tok->in = fd;
-				break ;
-			}
-			i++;
+			perror("ERROR");
+			return (errno);
 		}
+		if (par_tok->in[i + 1] == NULL)
+			break ;
+		i++;
 	}
+	exp_tok->in = fd;
+	return (EXIT_SUCCESS);
+}
+
+static int	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
+{
+	int		i;
+	int		fd;
+
 	i = 0;
 	fd = 1;
-	if (par_tok->out[i] != NULL)
+	while (true)
 	{
-		while (true)
+		if (par_tok->redir_type[is_pipe])
+			fd = -2;
+		if (par_tok->redir_type[is_out])
+			fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_TRUNC, 0644);
+		else if (par_tok->redir_type[is_out_append])
+			fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_APPEND, 0644);
+		if (fd == -1)
 		{
-			if (par_tok->redir_type[is_pipe])
-				fd = -2;
-			if (par_tok->redir_type[is_out])
-				fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_TRUNC, 0644);
-			else if (par_tok->redir_type[is_out_append])
-				fd = open(par_tok->out[i], O_RDWR | O_CREAT | O_APPEND, 0644);
-			if (fd == -1)
-			{
-				perror("ERROR");
-				return (errno);
-			}
-			if (par_tok->out[i + 1] == NULL)
-			{
-				exp_tok->out = fd;
-				break ;
-			}
-			i++;
+			perror("ERROR");
+			return (errno);
 		}
+		if (par_tok->out[i + 1] == NULL)
+			break ;
+		i++;
+	}
+	exp_tok->out = fd;
+	return (EXIT_SUCCESS);
+}
+
+static int	open_all_redirs(t_par_tok *par_tok, t_exp_tok *exp_tok)
+{
+	int	check;
+
+	if (par_tok->in[0] != NULL)
+	{
+		check = open_in(par_tok, exp_tok);
+		if (check != EXIT_SUCCESS)
+			return (check);
+	}
+	if (par_tok->out[0] != NULL)
+	{
+		check = open_out(par_tok, exp_tok);
+		if (check != EXIT_SUCCESS)
+			return (check);
 	}
 	return (EXIT_SUCCESS);
 }
