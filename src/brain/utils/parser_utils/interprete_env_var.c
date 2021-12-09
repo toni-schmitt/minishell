0c63b7e9d78f34a8e4c71a2bdb0a4fbfb942737f
Toni Schmitt <tonischmitt@online.de>
2021-12-09 20:36:18 +0100

    fix leak

--- src/brain/utils/parser_utils/interprete_env_var.c
+++ src/brain/utils/parser_utils/interprete_env_var.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 23:13:27 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/09 20:24:36 by toni             ###   ########.fr       */
+/*   Updated: 2021/12/09 20:34:41 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,10 @@ char	*interprete_env_var(char *lex_tok)
 	&& lex_tok[0] == '$')
 	{
 		if (ft_strstr(lex_tok, "$?"))
+		{
+			free(lex_tok);
 			return (ft_itoa(get_err_code()));
+		}
 		var = ft_substr(lex_tok, 1, ft_strlen(lex_tok));
 		if (var == NULL)
 			return (NULL);
