d67d909240159a67a7b37f27aa355f4133505572
Toni Schmitt <tonischmitt@online.de>
2021-12-09 19:54:09 +0100

    handle_redirs now calls executor and closes fds if not default

--- src/brain/utils/expander_utils/handle_redirs_utils.c
+++ src/brain/utils/expander_utils/handle_redirs_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_redirs_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:51:20 by tblaase           #+#    #+#             */
-/*   Updated: 2021/12/09 19:20:42 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/09 19:53:58 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,17 @@ static int	open_in(t_par_tok *par_tok, t_exp_tok *exp_tok)
 			fd = open(par_tok->in[i], O_RDONLY);
 		if (fd == -1)
 		{
-			dprintf(stderr, "something with %s is wrong\n", par_tok->in[i]);//remove after testing
+			// fprintf(stderr, "something with %s is wrong\n", par_tok->in[i]);//remove after testing
 			perror("ERROR_in");//change this after finished debugging to ERROR
 			return (EXIT_FAILURE);
 		}
 		if (par_tok->in[i + 1] == NULL)
 			break ;
+		close(fd);
 		i++;
 	}
 	exp_tok->in = fd;
-	dprintf(stderr, "the new fd for input is now %d\n", exp_tok->in);//remove after testing
+	// fprintf(stderr, "the new fd for input is now %d\n", exp_tok->in);//remove after testing
 	return (EXIT_SUCCESS);
 }
 
@@ -63,21 +64,29 @@ static int	open_out(t_par_tok *par_tok, t_exp_tok *exp_tok)
 		}
 		if (par_tok->out[i + 1] == NULL)
 			break ;
+		close(fd);
 		i++;
 	}
 	exp_tok->out = fd;
-	dprintf(stderr, "the new fd for output is now %d\n", exp_tok->out);//remove after testing
+	// fprintf(stderr, "the new fd for output is now %d\n", exp_tok->out);//remove after testing
 	return (EXIT_SUCCESS);
 }
 
 int	handle_redir(t_par_tok *par_tok, t_exp_tok *exp_tok)//remove comments after testing
 {
-	dprintf(stderr, "entering handle_redir\n");
+	int	exit_status;
+
+	// fprintf(stderr, "entering handle_redir\n");
 	if (open_in(par_tok, exp_tok) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	dprintf(stderr, "successfully set in redirection\n");
+	// fprintf(stderr, "successfully set in redirection\n");
 	if (open_out(par_tok, exp_tok) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	dprintf(stderr, "successfully set out redirection\n");
-	return (EXIT_SUCCESS);
+	// fprintf(stderr, "successfully set out redirection\n");
+	exit_status = executor(exp_tok);
+	if (exp_tok->in != STDIN_FILENO)
+		close(exp_tok->in);
+	if (exp_tok->out != STDOUT_FILENO)
+		close(exp_tok->out);
+	return (exit_status);
 }
