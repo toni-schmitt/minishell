fc4d30ef7f20b88e93bd3b62a8046a66f2d0c5fe
Toni Schmitt <tonischmitt@online.de>
2021-12-10 19:41:12 +0100

    now interpretes wildcards too

--- src/brain/utils/parser_utils/interprete_env_var.c
+++ src/brain/utils/parser_utils/interprete_env_var.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 23:13:27 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/09 20:34:41 by toni             ###   ########.fr       */
+/*   Updated: 2021/12/10 19:38:38 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,71 @@
 #include "parser_utils.h"
 #include "env_var_utils.h"
 
-// char	*get_env_var_value(char **env_var, char *var);
+static char	**get_next_item(char *n, char *items[], size_t *it_siz, size_t *i)
+{
+	if (*it_siz == (*i + 2))
+	{
+		*it_siz += 10;
+		items = ft_realloc_str_arr(items, *it_siz);
+		if (items == NULL)
+			return (NULL);
+	}
+	items[*i] = ft_strdup(n);
+	if (items[*i] == NULL)
+		return (ft_free_split(items));
+	(*i)++;
+	return (items);
+}
+
+static char	*get_items_sorted(char **unsorted)
+{
+	char	*items;
+	int		i;
+
+	unsorted = ft_sort_str_arr(unsorted);
+	i = 0;
+	while (unsorted[i])
+	{
+		items = ft_append(&items, unsorted[i]);
+		if (items == NULL)
+			return (ft_free_split(unsorted));
+		items = ft_append(&items, " ");
+		if (items == NULL)
+			return (ft_free_split(unsorted));
+		i++;
+	}
+	ft_free_split(unsorted);
+	return (items);
+}
+
+static char	*get_dir_items(void)
+{
+	DIR				*d;
+	struct dirent	*dir;
+	char			**items;
+	size_t			items_size;
+	size_t			i;
+
+	d = opendir("/home/toni/repos/minishell");
+	if (d == NULL)
+		return (NULL);
+	i = 0;
+	items_size = 10;
+	dir = readdir(d);
+	items = ft_calloc(items_size + 1, sizeof(*items));
+	if (items == NULL)
+		return (NULL);
+	while (dir != NULL)
+	{
+		if (dir->d_name[0] != '.')
+			items = get_next_item(dir->d_name, items, &items_size, &i);
+		if (items == NULL)
+			return (NULL);
+		dir = readdir(d);
+	}
+	closedir(d);
+	return (get_items_sorted(items));
+}
 
 /**
  * @brief  Interprets enviroment variables of current lexer token
@@ -29,6 +93,7 @@ char	*interprete_env_var(char *lex_tok)
 	char	*var;
 
 	interpreted_token = lex_tok;
+	printf("%s\n", lex_tok);
 	if (!ft_strchr(lex_tok, '\'') && ft_strlen(lex_tok) > 1 \
 	&& lex_tok[0] == '$')
 	{
@@ -46,5 +111,7 @@ char	*interprete_env_var(char *lex_tok)
 		if (interpreted_token == NULL)
 			return (NULL);
 	}
+	if (!ft_strchr(lex_tok, '\'') && ft_strchr(lex_tok, '*'))
+		return (get_dir_items());
 	return (interpreted_token);
 }
