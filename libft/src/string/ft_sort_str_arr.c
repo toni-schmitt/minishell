0299169ee0342147e1392b97eaf7b47d4fe95a3c
Toni Schmitt <tonischmitt@online.de>
2021-12-10 19:40:32 +0100

    add ft_sort_str_arr.c

--- libft/src/string/ft_sort_str_arr.c
+++ libft/src/string/ft_sort_str_arr.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 18:59:15 by toni              #+#    #+#             */
-/*   Updated: 2021/12/10 19:35:41 by toni             ###   ########.fr       */
+/*   Updated: 2021/12/10 19:39:57 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ static void	swap(char **old, char **new)
 
 char	**ft_sort_str_arr(char *unsorted[])
 {
-	char	*tmp;
 	int		unsorted_len;
 	int		i;
 	int		j;
@@ -31,14 +30,13 @@ char	**ft_sort_str_arr(char *unsorted[])
 	if (unsorted == NULL)
 		return (NULL);
 	unsorted_len = ft_str_arr_len(unsorted);
-	tmp = NULL;
 	i = 0;
 	while (i < unsorted_len)
 	{
 		j = 0;
 		while (j < (unsorted_len - 1 - i))
 		{
-			if (strcmp(unsorted[j], unsorted[j + 1]) > 0)
+			if (ft_strcmp(unsorted[j], unsorted[j + 1]) > 0)
 				swap(&unsorted[j], &unsorted[j + 1]);
 			j++;
 		}
