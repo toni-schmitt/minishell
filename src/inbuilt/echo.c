efadb6f4e58c3b8110815d2969998eca667c1957
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-12-13 16:04:01 +0100

    fix error when error gets empty string as arguments

--- src/inbuilt/echo.c
+++ src/inbuilt/echo.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 21:33:39 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/11 16:51:00 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/12/13 15:56:57 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	echo(char **args)
 	{
 		if (printf("%s", *args) != ft_strlen(*args))
 			return (EXIT_FAILURE);
-		if (*(args + 1) != NULL)
+		if (ft_strlen(*args) != 0 && *(args + 1) != NULL)
 			if (printf(" ") != ft_strlen(" "))
 				return (EXIT_FAILURE);
 		args++;
