b7d9db01f00f54dbc7200d00fb98c76b8c659cd3
tblaase <tblaase@student.42heilbronn.de>
2021-12-13 15:43:10 +0100

    removed comment

--- libft/src/io/ft_puterror.c
+++ libft/src/io/ft_puterror.c
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 20:42:48 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/13 13:39:11 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/13 15:42:56 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_puterror(char *message)
 {
 	ft_printf("%s\n", message);
-	exit(EXIT_FAILURE); // why exit and return?????
+	exit(EXIT_FAILURE);
 	return (EXIT_FAILURE);
 }
