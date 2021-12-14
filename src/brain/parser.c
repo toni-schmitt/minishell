356f1e007b445d435d03f20d4268ec5b81b9eeb5
Toni Schmitt <tonischmitt@online.de>
2021-12-14 17:25:59 +0100

    remove fix norm

--- src/brain/parser.c
+++ src/brain/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:35:35 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/11 13:19:58 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/12/14 17:25:33 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,96 +93,9 @@ static char	**interprete_env_vars(char *lex_toks[])
 	return (lex_toks);
 }
 
-void prnt_token(t_par_tok *tok[])
-{
-	char *white = "\033[0;37m";
-	char *red = "\033[0;31m";
-	char *green = "\033[0;32m";
-	char *purp = "\033[0;35m";
-	char *boldred = "\033[1;31m";
-	char *boldwhite = "\033[1;37m";
-	for (int i = 0; tok[i]; i++)
-	{
-		printf("%sToken [%d]:\n%s", boldwhite, i, white);
-		if (tok[i]->type != 0)
-			printf("%sType: %s", purp, white);
-		switch (tok[i]->type)
-		{
-			case 1:
-				printf("subshell\n");
-				break;
-			case 2:
-				printf("%s&&%s\n", boldwhite, white);
-				break;
-			case 3:
-				printf("%s||%s\n", boldwhite, white);
-				break;
-			default:
-				if (tok[i]->type != 0)
-					printf("%s%s%d is unsepcified\n%s", boldred, red, tok[i]->type, white);
-				break;
-		}
-		for (int m = 0; m < 5; m++)
-			if (tok[i]->redir_type[m])
-			{
-				printf("%sRedirections:\n%s", purp, white);
-				break;
-			}
-		for (int m = 0; m < 5; m++)
-		{
-			if (tok[i]->redir_type[m])
-			{
-				switch (m)
-				{
-					case 0:
-						printf("Is Pipe:\t");
-						break;
-					case 1:
-						printf("Is In:\t\t");
-						break;
-					case 2:
-						printf("Is Heredoc:\t");
-						break;
-					case 3:
-						printf("Is Out:\t\t");
-						break;
-					case 4:
-						printf("Is Out-Append:\t");
-						break;
-					default:
-						break;
-				}
-				printf("%strue\n%s", green, white);
-			}
-		}
-		if (tok[i]->cmd)
-		{
-			printf("%sCommand:\n%s", purp, white);
-			for (int j = 0; tok[i]->cmd && tok[i]->cmd[j]; j++)
-				printf("%s\t", tok[i]->cmd[j]);
-			printf("\n");
-		}
-		if (tok[i]->in)
-		{
-			printf("%sIn Redirections:\n%s", purp, white);
-			for (int k = 0; tok[i]->in && tok[i]->in[k]; k++)
-				printf("%s\t", tok[i]->in[k]);
-			printf("\n");
-		}
-		if (tok[i]->out)
-		{
-			printf("%sOut Redirections:\n%s", purp, white);
-			for (int l = 0; tok[i]->out && tok[i]->out[l]; l++)
-				printf("%s\t", tok[i]->out[l]);
-			printf("\n");
-		}
-		printf("\n");
-	}
-}
-
 int	parser(char *lexer_tokens[])
 {
-	t_par_tok	**tokens = NULL;
+	t_par_tok	**tokens;
 	int			exit_code;
 
 	lexer_tokens = interprete_env_vars(lexer_tokens);
@@ -195,6 +108,5 @@ int	parser(char *lexer_tokens[])
 	if (exit_code == EXIT_SYNTAX_ERROR)
 		return (EXIT_SUCCESS);
 	tokens = get_par_toks();
-	// prnt_token(tokens);
 	return (free_par_toks(tokens, expander(tokens)));
 }
