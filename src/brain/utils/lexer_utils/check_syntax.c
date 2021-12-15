2c5b2937d44b233bcc9378a9071bd16a34ddf06f
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-12-16 00:30:59 +0100

    is_special now has special_type to check specific types of specials

--- src/brain/utils/lexer_utils/check_syntax.c
+++ src/brain/utils/lexer_utils/check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:00:19 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/16 00:02:49 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/12/16 00:09:27 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,23 @@ static bool	is_redir(char *lex_tok)
 	return (false);
 }
 
-static bool	is_special(char *lex_tok)
+static bool	is_special(char *lex_tok, int special_kind)
 {
-	if (ft_strlen(lex_tok) == 1)
+	if (ft_strlen(lex_tok) == 1 && special_kind == 0)
 	{
 		if ((ft_strchr(lex_tok, '<') && !ft_strstr(lex_tok, "<<")) \
 		|| (ft_strchr(lex_tok, '>') && !ft_strstr(lex_tok, ">>")) \
 		|| (ft_strchr(lex_tok, '|') && !ft_strstr(lex_tok, "||")))
 			return (true);
 	}
-	if (ft_strlen(lex_tok) == 2)
+	if (ft_strlen(lex_tok) == 2 && special_kind == 0)
 	{
-		if (ft_strstr(lex_tok, "&&") || ft_strstr(lex_tok, "||") \
-		|| ft_strstr(lex_tok, "<<") || ft_strstr(lex_tok, ">>"))
+		if (ft_strstr(lex_tok, "<<") || ft_strstr(lex_tok, ">>"))
+			return (true);
+	}
+	if (ft_strlen(lex_tok) == 2 && special_kind == 3)
+	{
+		if (ft_strstr(lex_tok, "&&") || ft_strstr(lex_tok, "||"))
 			return (true);
 	}
 	return (false);
@@ -113,7 +117,7 @@ bool	is_valid_syntax(char *lex_toks[])
 			if (!is_correc_pipe(lex_toks[i], lex_toks[i + 1]))
 				return (false);
 			if (ft_strchr(lex_toks[i], '(') && ft_strchr(lex_toks[i], ')'))
-				if (i != 0 && is_special(lex_toks[i - 1]))
+				if (i != 0 && is_special(lex_toks[i - 1], 0))
 					return (false);
 		}
 		i++;
