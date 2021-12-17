a59cb3015159f670c562f75b2937f1811ec43652
Toni Schmitt <tonischmitt@online.de>
2021-12-17 16:15:19 +0100

    add basic syntax checking rules for line

--- src/brain/utils/lexer_utils/check_line_syntax.c
+++ src/brain/utils/lexer_utils/check_line_syntax.c
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 15:42:45 by toni              #+#    #+#             */
-/*   Updated: 2021/12/17 16:03:26 by toni             ###   ########.fr       */
+/*   Updated: 2021/12/17 16:13:23 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,39 @@ int	exit_on_syntax_error(void)
 	return (EXIT_SYNTAX_ERROR);
 }
 
+static int	jump_to_end_of_symbol(char *line, char symbol, int i)
+{
+	while (line[i] && line[i] != symbol)
+		i++;
+	return (i);
+}
+
+static bool	is_correct_subshell(char *line, int i)
+{
+	while (line[i] && ft_isspace(line[i]))
+		i--;
+	if (line[i] != '&' || line[i] != '|')
+		return (false);
+	return (true);
+}
+
 bool	is_valid_line_syntax(char *line)
 {
-	(void)line;
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\'')
+			i = jump_to_end_of_symbol(line, '\'', i);
+		if (line[i] == '\"')
+			i = jump_to_end_of_symbol(line, '\"', i);
+		if (line[i] == '(')
+			if (!is_correct_subshell(line, i))
+				return (false);
+		if (line[i] == ';' || line[i] == '\\')
+			return (false);
+		i++;
+	}
 	return (true);
 }
