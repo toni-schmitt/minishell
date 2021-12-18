3dfb5d503422725c6c9864e3bafad48bc56a0239
Toni Schmitt <tonischmitt@online.de>
2021-12-18 21:34:49 +0100

    exit now behaves more like bash exit - now prints everythin to stderr - now supports long instead of int as exit code - now supports negative arguments

--- src/inbuilt/exit.c
+++ src/inbuilt/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 13:22:09 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/12/16 21:56:21 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/12/18 21:33:10 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static bool	is_valid_exit_arg(char *args[])
 		j = 0;
 		while (args[i][j])
 		{
+			if (ft_issign(args[i][j]))
+				j++;
 			if (!ft_isdigit(args[i][j]))
 				return (false);
 			j++;
@@ -34,18 +36,18 @@ static bool	is_valid_exit_arg(char *args[])
 
 int	exit_inbuilt(char *args[])
 {
-	int	exit_code;
-	int	i;
+	long	exit_code;
+	int		i;
 
 	i = 0;
 	if (ft_strncmp(args[i], "exit", ft_strlen(args[i])) != 0)
 		return (EXIT_FAILURE);
 	i++;
 	if (args[i])
-		exit_code = ft_atoi(args[i]);
+		exit_code = ft_atol(args[i]);
 	else
 		exit_code = get_err_code();
-	if (!is_valid_exit_arg(args + i))
+	if (!is_valid_exit_arg(args + i) || ft_strlen(args[i]) > 19)
 	{
 		ft_fprintf(STDERR_FILENO, "exit: not a valid argument\n");
 		exit_code = 255;
@@ -56,7 +58,7 @@ int	exit_inbuilt(char *args[])
 		printf("exit\n");
 		return (EXIT_FAILURE);
 	}
-	printf("exit\n");
+	ft_fprintf(STDERR_FILENO, "exit\n");
 	exit(exit_code);
 	return (exit_code);
 }
